1cfecd84c04d3519f6cf8cfd2c5adf2c7a41ad4a
Denis_Mazilo <denisround@icloud.com>
2025-03-06 20:16:44 +0100

    refactor-script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener(() => {
+    const container = document.createElement("div");
+    container.classList.add("container");
+    document.body.appendChild(container);
+
+    const columns = ["ID", "KIND", "BODY"];
+
+    columns.forEach(title => {
+        const yellowDiv = document.createElement("div");
+        yellowDiv.classList.add("yellow");
+
+        const header = document.createElement("div");
+        header.classList.add("header");
+        const h1 = document.createElement("h1");
+        h1.textContent = title;
+        header.appendChild(h1);
+
+        const blueDiv = document.createElement("div");
+        blueDiv.classList.add("blue");
+
+        yellowDiv.appendChild(header);
+        yellowDiv.appendChild(blueDiv);
+        container.appendChild(yellowDiv);
+    });
+});
