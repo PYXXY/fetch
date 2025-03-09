4a3dc44e68b7b0433482bfa0967aaab7c485151d
Denis_Mazilo <denisround@icloud.com>
2025-03-09 17:02:52 +0100

    refactor-error-deleted

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-document.addEventListener(() => {
-    const container = document.createElement("div");
-    container.classList.add("container");
-    document.body.appendChild(container);
+const container = document.createElement("div");
+container.classList.add("container");
+document.body.appendChild(container);
 
-    const columns = ["ID", "KIND", "BODY"];
+const columns = ["ID", "KIND", "BODY"];
 
-    columns.forEach(title => {
-        const yellowDiv = document.createElement("div");
-        yellowDiv.classList.add("yellow");
+columns.forEach((title) => {
+  const yellowDiv = document.createElement("div");
+  yellowDiv.classList.add("yellow");
 
-        const header = document.createElement("div");
-        header.classList.add("header");
-        const h1 = document.createElement("h1");
-        h1.textContent = title;
-        header.appendChild(h1);
+  const header = document.createElement("div");
+  header.classList.add("header");
+  const h1 = document.createElement("h1");
+  h1.textContent = title;
+  header.appendChild(h1);
 
-        const blueDiv = document.createElement("div");
-        blueDiv.classList.add("blue");
+  const blueDiv = document.createElement("div");
+  blueDiv.classList.add("blue");
 
-        yellowDiv.appendChild(header);
-        yellowDiv.appendChild(blueDiv);
-        container.appendChild(yellowDiv);
-    });
+  yellowDiv.appendChild(header);
+  yellowDiv.appendChild(blueDiv);
+  container.appendChild(yellowDiv);
 });
